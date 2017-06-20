0d390357ae394d88a6cfae0ceed44ed3eec769fa
eduardo morango <jevmor@gmail.com>
2017-06-20 16:11:45 -0300

    Identation

--- Sources/WSRequest.swift
+++ Sources/WSRequest.swift
@@ -8,44 +8,27 @@
 
 import Foundation
 
-
-
 public class WSRequest: WSOperation{
-
 
     let url: URL
     let session : URLSession
     let completionHandler: (Data?, URLResponse?, Error?) -> ()
-    
-
-   public init(url: URL,session: URLSession, completionHandler: @escaping (Data?, URLResponse?, Error?) -> () ) {
-   
+    private weak var task: URLSessionTask?
+    public init(url: URL,session: URLSession, completionHandler: @escaping (Data?, URLResponse?, Error?) -> () ) {
         self.url = url
         self.session = session
         self.completionHandler = completionHandler
-        
         super.init()
-    
-    
-    
     }
-    
-    
-    private weak var task: URLSessionTask?
     
     override public func main() {
         
-         self.task =  session.dataTask(with: url){ data,response,error in
-            
+        self.task =  session.dataTask(with: url){ data,response,error in   
             self.completionHandler(data,response,error)
             self.completeOperation()
-        
         }
         
-        
         self.task!.resume()
-        
-        
     }
     
     override public func cancel() {
@@ -53,9 +36,4 @@ public class WSRequest: WSOperation{
         super.cancel()
     }
     
-    
-    
-    
-    
-
 }
